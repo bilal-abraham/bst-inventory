eaba7b06670a366fb3811a4998d73d597c5f445f
bilal-abraham <bilal.abraham.717@gmail.com>
2022-01-25 18:22:30 -0800

    averaging and sum functions complete

--- Main.js
+++ Main.js
@@ -9,10 +9,14 @@ let bst = new BST(comparatorFunction);
 
 for (let i = 0; i < lines.length; i++) {
 	lines[i] = JSON.parse(lines[i]);
+	lines[i].cost = parseInt(lines[i].cost.replace('$', ''));
+
 	bst.add(lines[i]);
 }
 
 const inOrderLines = bst.inOrder();
+
+console.log(inOrderLines);
 
 const groupBy = (arr, key) =>
 	arr.reduce(
@@ -20,11 +24,30 @@ const groupBy = (arr, key) =>
 		{}
 	);
 
-console.log(groupBy(inOrderLines, 'name'));
+// let output = groupBy(inOrderLines, 'name');zxsx
+// console.log(output);
 
-// let output = groupBy(inOrderLines);
+const reduced = inOrderLines.reduce((p, c) => {
+	if (!p[c.name]) {
+		p[c.name] = { ...c, count: 1 };
+		return p;
+	}
+	p[c.name].stock += c.stock;
+	p[c.name].cost += c.cost;
+	p[c.name].count += 1;
+	return p;
+}, {});
 
-// for (const line of output)
-// 	writeFileSync('data/storeData.txt', JSON.stringify(line) + '\n', {
-// 		flag: 'a+',
-// 	});
+const result = Object.keys(reduced).map((x) => {
+	const item = reduced[x];
+	return {
+		name: item.name,
+		stock: item.stock,
+		cost: item.cost / item.count,
+	};
+});
+
+for (const line of result)
+	writeFileSync('data/storeData.txt', JSON.stringify(line) + '\n', {
+		flag: 'a+',
+	});
